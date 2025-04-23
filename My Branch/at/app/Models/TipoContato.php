6a7aec28ee3799a32cbf895e83970094739a47a6
FelipeMacedoK <felipe.macedo.0507@gmail.com>
2025-04-23 19:45:11 -0300

    New Files

---
+++ My Branch/at/app/Models/TipoContato.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TipoContato extends Model
+{
+    protected $fillable = [
+        'nome',
+        'descricao',
+    ];
+
+    public function contatos()
+    {
+        return $this->hasMany(Contato::class);
+    }
+}
